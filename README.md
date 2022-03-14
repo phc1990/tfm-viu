3361a1979a7ba4230d750068853562b02b80007e
Pau Hebrero <pauhebrero@gmail.com>
2022-03-14 20:20:02 +0100

    XSA HTTP Download method.

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # tfm-viu
+
+Trabajo Fin de Master (TFM) for the Valencian International University (VIU).
+
+## Layout
+
+The repository presents the following layout:
+
+```
+/
+    .gitignore      
+    python          Python3 source code and test data
+```
+
+## `/python`
+
+The `/python` module contains [Python3](https://www.python.org/downloads/) source code and test data:
+
+```
+/src                source code
+/test               test code
+    /data           test data
+```
+
+The main functionalities of the module are:
+- Interfacing with XMM-Newton Science Archive (XSA) for data download.
+
+### Style
+
+Both source and test files follow the [PEP 8 Style Guide](https://peps.python.org/pep-0008/#introduction).
+
+### Tests
+
+Test files use the [unittest](https://docs.python.org/3/library/unittest.html) framework.
+
+Tests can be run from the `/python` directory:
+
+```
+python3 -m unittest discover test/
+```
+
+
---
+++ python/test/test_utils.py
@@ -0,0 +1,44 @@
+import os
+import tempfile
+import unittest
+import src.utils as utils
+import test.helper as helper
+import tarfile
+
+class TestUtils(unittest.TestCase):
+    
+    def test_find_in_tar_0_matches(self):
+        tar_path = helper.get_test_data_file_path(file_name='tar1.tar')
+        
+        with tarfile.open(name=tar_path, mode='r') as tar: 
+            matches = utils.find_members_in_tar(tar=tar,
+                                    regex_pattern='^.*?CRAZY_PATTERN_TO_MATCH$')
+        
+            self.assertEqual(len(matches),0)
+    
+    def test_find_in_tar_2_matches(self):
+        tar_path = helper.get_test_data_file_path(file_name='tar1.tar')
+        
+        with tarfile.open(name=tar_path, mode='r') as tar: 
+            matches = utils.find_members_in_tar(tar=tar,
+                                        regex_pattern='^.*?FSIMAG.*?\.FTZ$')
+            
+            self.assertEqual(len(matches),2)
+        
+    def test_extract_tar_members_to_dir(self):
+        with tempfile.TemporaryDirectory() as output_dir:
+            tar_path = helper.get_test_data_file_path(file_name='tar1.tar')
+            
+            with tarfile.open(name=tar_path, mode='r') as tar: 
+                members = utils.find_members_in_tar(tar=tar,
+                                                    regex_pattern='.')
+            
+                for member in members:
+                    utils.extract_tar_member_to_dir(tar=tar,
+                                                    member=member,
+                                                    output_dir=output_dir)
+                    
+                self.assertEqual(len(os.listdir(output_dir)), 6)
+                    
+                
+            
