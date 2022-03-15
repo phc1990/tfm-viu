9b60794d70848f947fb54d5458f43eb683df7ac9
Pau Hebrero <pauhebrero@gmail.com>
2022-03-15 18:51:14 +0100

    Renamed classes

--- python/test/test_fits.py
+++ python/test/test_fits.py
@@ -3,17 +3,18 @@ import src.fits as fits
 import test.helper as helper
 import time
 
+
 class TestDs9Ui(unittest.TestCase):
     
     def test_display_1_fits(self):
-        ds9_ui = fits.FitsDs9Ui(ds9_path=helper.get_test_data_file_path('ds9'))
+        ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'))
         
         ds9_ui.display(fits_paths=[helper.get_test_data_file_path('fits1.fits')])
         time.sleep(15)
         ds9_ui.close_current_display()
         
     def test_display_1_fits_with_zoom_to_fit_and_zscale(self):
-        ds9_ui = fits.FitsDs9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
+        ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
                                 zoom='to fit',
                                 zscale=True)
         
@@ -22,14 +23,14 @@ class TestDs9Ui(unittest.TestCase):
         ds9_ui.close_current_display()
         
     def test_display_1_ftz(self):
-        ds9_ui = fits.FitsDs9Ui(ds9_path=helper.get_test_data_file_path('ds9'))
+        ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'))
         
         ds9_ui.display(fits_paths=[helper.get_test_data_file_path('fits1.ftz')])
         time.sleep(15)
         ds9_ui.close_current_display()
         
     def test_display_1_ftz_with_zoom_to_fit_and_zscale(self):
-        ds9_ui = fits.FitsDs9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
+        ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
                                 zoom='to fit',
                                 zscale=True)
         
@@ -38,7 +39,7 @@ class TestDs9Ui(unittest.TestCase):
         ds9_ui.close_current_display()
         
     def test_display_1_fits_and_1_ftz_with_zoom_to_fit_and_zscale(self):
-        ds9_ui = fits.FitsDs9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
+        ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
                                 zoom='to fit',
                                 zscale=True)
         
