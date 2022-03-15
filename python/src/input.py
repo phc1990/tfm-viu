641b9b30eb4d5054edbfc7d0f30c66d87acf6a1e
Pau Hebrero <pauhebrero@gmail.com>
2022-03-15 18:59:10 +0100

    Added input module.

---
+++ python/src/input.py
@@ -0,0 +1,77 @@
+"""Input module.
+
+Used to collect/listen to analysis input.
+"""
+
+
+from email import message
+from enum import Enum
+
+
+class Input(Enum):
+    """Subset of available inputs."""
+    DETECTED        = 1
+    DUBIOUS         = 2
+    NOT_DETECTED    = 3
+    REPEAT          = 4
+    CLOSE           = 5
+
+
+class InputInterface:
+    """Input interface."""
+    
+    def message(self, message: str):
+        """Sends a message to the input interface.
+
+        Args:
+            message (str): message to be displayed
+        """
+        pass
+    
+    def listen(self) -> Input:
+        """Waits and listens for inputs.
+
+        Returns:
+            Input: received input
+        """
+        pass
+    
+    
+class StdIoUi(InputInterface):
+    """Input interface based on standard input ('stdin') and 
+    standard output ('stdout').
+    """
+    
+    _DETECTED       = ["y", "yes", "Y", "YES"]
+    _DUBIOUS        = ["d", "D"]
+    _NOT_DETECTED   = ["n", "no", "N", "NO"]
+    _REPEAT         = ["r", "repeat", "R", "REPEAT"]
+    _CLOSE          = ["c", "close", "C", "CLOSE"]
+    
+    def listen(self) -> Input:
+        """See base class."""
+        input = input()
+        
+        if input in self._DETECTED:
+            return Input.DETECTED
+        elif input in self._DUBIOUS:
+            return Input.DUBIOUS
+        elif input in self._NOT_DETECTED:
+            return Input.NOT_DETECTED
+        elif input in self._REPEAT:
+            return Input.REPEAT
+        elif input in self._CLOSE:
+            return Input.CLOSE
+        else:
+            message('Invalid input. Available inputs:')
+            message('Detection    : ' + ' '.join(self._DETECTED))
+            message('Dubious      : ' + ' '.join(self._DUBIOUS))
+            message('No detection : ' + ' '.join(self._NOT_DETECTED))
+            message('Repeat       : ' + ' '.join(self._REPEAT))
+            message('Close        : ' + ' '.join(self._CLOSE))
+            return self.listen()
+            
+    def message(self, message: str):
+        """See base class."""
+        print(message)
+    
