39626e4d84944a4a9db5f2ef10452186c1f4b4d7
Pau Hebrero <pauhebrero@gmail.com>
2022-03-15 18:52:37 +0100

    Formatting

--- python/test/test_fits.py
+++ python/test/test_fits.py
@@ -15,8 +15,8 @@ class TestDs9Ui(unittest.TestCase):
         
     def test_display_1_fits_with_zoom_to_fit_and_zscale(self):
         ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
-                                zoom='to fit',
-                                zscale=True)
+                            zoom='to fit',
+                            zscale=True)
         
         ds9_ui.display(fits_paths=[helper.get_test_data_file_path('fits1.fits')])
         time.sleep(15)
@@ -31,8 +31,8 @@ class TestDs9Ui(unittest.TestCase):
         
     def test_display_1_ftz_with_zoom_to_fit_and_zscale(self):
         ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
-                                zoom='to fit',
-                                zscale=True)
+                            zoom='to fit',
+                            zscale=True)
         
         ds9_ui.display(fits_paths=[helper.get_test_data_file_path('fits1.ftz')])
         time.sleep(15)
@@ -40,8 +40,8 @@ class TestDs9Ui(unittest.TestCase):
         
     def test_display_1_fits_and_1_ftz_with_zoom_to_fit_and_zscale(self):
         ds9_ui = fits.Ds9Ui(ds9_path=helper.get_test_data_file_path('ds9'),
-                                zoom='to fit',
-                                zscale=True)
+                            zoom='to fit',
+                            zscale=True)
         
         ds9_ui.display(fits_paths=[
             helper.get_test_data_file_path('fits1.fits'),
